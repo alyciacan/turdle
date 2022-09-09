059c4b6c17dfcfb65e84b62961480d9b214de84d
Alycia Canavan <alycia.canavan@gmail.com>
2022-09-09 13:29:03 -0700

    Set up fetch fn and reorganized for asynchronous JS

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var gameOverGuessCount = document.querySelector('#game-over-guesses-count');
 var gameOverGuessGrammar = document.querySelector('#game-over-guesses-plural');
 
 // Event Listeners
-window.addEventListener('load', setGame);
+window.addEventListener('load', getRandomWord);
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener('keyup', function() { moveToNextInput(event) });
@@ -40,15 +40,25 @@ viewGameButton.addEventListener('click', viewGame);
 viewStatsButton.addEventListener('click', viewStats);
 
 // Functions
-function setGame() {
+
+const fetchData = fetch('http://localhost:3001/api/v1/words')
+    .then(response => response.json())
+    .then(wordArray => wordArray)
+    .catch(error => alert("There was an error!"));
+
+
+function setGame() { //called on load, doesn't have the array yet
   currentRow = 1;
-  winningWord = getRandomWord();
+  console.log(winningWord);
   updateInputPermissions();
 }
 
 function getRandomWord() {
-  var randomIndex = Math.floor(Math.random() * 2500);
-  return words[randomIndex];
+  fetchData.then((n) => {
+    var randomIndex = Math.floor(Math.random() * n.length);
+    winningWord = n[randomIndex];
+    setGame()
+  })
 }
 
 function updateInputPermissions() {
@@ -188,7 +198,7 @@ function changeGameOverText() {
 function startNewGame() {
   clearGameBoard();
   clearKey();
-  setGame();
+  getRandomWord();
   viewGame();
   inputs[0].focus();
 }
